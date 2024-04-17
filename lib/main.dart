448bdf421727c0b749d05450a54a661811c7faf6
Fabrizio Mattos <fmattos@unsa.edu.pe>
2024-04-16 23:49:37 -0500

    Added search bar to home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app_practice/screens/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // SystemChrome.setSystemUIOverlayStyle(
+    //     const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'ToDo App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
